b3fc9c1abbb69674a3f84764f68155710c1adf79
Talgat <JorobekovT@gmail.com>
2025-12-26 17:38:49 +0600

    реализовал viewки для api_v1

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.http import HttpStatus
 
-# Create your views here.
+
+def add(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        A = data.get('A')
+        B = data.get('B')
+
+        if not isinstance(A, (int, float)) or not isinstance(B, (int, float)):
+            return JsonResponse(
+                {"error": "Введите цифры."},
+                status=HttpStatus.BAD_REQUEST
+            )
+
+        answer = A + B
+        return JsonResponse({"answer": answer})
+
+
+def subtract(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        A = data.get('A')
+        B = data.get('B')
+
+        if not isinstance(A, (int, float)) or not isinstance(B, (int, float)):
+            return JsonResponse(
+                {"error": "Введите цифры."},
+                status=HttpStatus.BAD_REQUEST
+            )
+
+        answer = A - B
+        return JsonResponse({"answer": answer})
+
+
+def multiply(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        A = data.get('A')
+        B = data.get('B')
+
+        if not isinstance(A, (int, float)) or not isinstance(B, (int, float)):
+            return JsonResponse(
+                {"error": "Введите цифры."},
+                status=HttpStatus.BAD_REQUEST
+            )
+
+        answer = A * B
+        return JsonResponse({"answer": answer})
+
+
+def divide(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        A = data.get('A')
+        B = data.get('B')
+
+        if not isinstance(A, (int, float)) or not isinstance(B, (int, float)):
+            return JsonResponse(
+                {"error": "Введите цифры."},
+                status=HttpStatus.BAD_REQUEST
+            )
+
+        if B == 0:
+            return JsonResponse(
+                {"error": "Нельзя делить на ноль."},
+                status=HttpStatus.BAD_REQUEST
+            )
+
+        answer = A / B
+        return JsonResponse({"answer": answer})
